2916410d5b55ccd0364e0cff5394a508d5df2dc3
camilleherrera <herreracamillec@gmail.com>
2021-08-28 04:24:45 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,11 +49,10 @@ sr.reveal('.about__text', { delay: 100 });
 sr.reveal('.skills__subtitle', {});
 sr.reveal('.skills__text', {});
 sr.reveal('.skills__data', { interval: 50 });
-sr.reveal('.skills__img', { delay: 600 });
+sr.reveal('.skills__img', { delay: 50 });
 
 /*SCROLL WORK*/
 sr.reveal('.work__img', { interval: 50 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input', { interval: 50 });
 sr.reveal('#contact-form', { interval: 50 });
